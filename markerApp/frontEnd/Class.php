f6a830305fdeda712974c853d139999e3762ae80
LawrenceChen15 <lawrence.chn15@gmail.com>
2023-01-11 23:35:53 -0500

    Keep yes

--- markerApp/frontEnd/Class.php
+++ markerApp/frontEnd/Class.php
@@ -61,6 +61,75 @@ body {
   <a href="#contact">Contact</a>
 </div>
    
+
+<head>
+<style>
+.grid-container {
+  display: grid;
+  row-gap: 50px;
+  grid-template-columns: auto auto auto;
+  background-color: #2196F3;
+  padding: 10px;
+}
+
+.body-center{
+  margin-left: 13%;
+}
+
+.grid-item {
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  padding: 20px;
+  font-size: 30px;
+  text-align: center;
+}
+
+#drag1, #drag2, #drag3, #drag4{
+  float:left;
+}
+</style>
+</head>
+<body class = "body-center">
+
+<div ondrop="drop(event)" ondragover="allowDrop(event)" class="grid-container">
+  <div class="grid-item"></div>
+  <div class="grid-item"></div>
+  <div class="grid-item"></div>  
+  <div class="grid-item"></div>
+  <div class="grid-item"></div>
+  <div class="grid-item"></div>  
+  <div class="grid-item"></div>
+  <div class="grid-item"></div>
+  <div class="grid-item"></div>  
+</div>
+
+<script>
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+</script>
+</head>
+<body>
+<div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+<br>
+<img id="drag1" src="/markerApp/markerApp/Pictures/Circle1.png" draggable="true" ondragstart="drag(event)" width="69" height="69">
+<img id="drag2" src="/markerApp/markerApp/Pictures/Circle2.png" draggable="true" ondragstart="drag(event)" width="69" height="69">
+<img id="drag3" src="/markerApp/markerApp/Pictures/Circle3.png" draggable="true" ondragstart="drag(event)" width="69" height="69">
+<img id="drag4" src="/markerApp/markerApp/Pictures/Circle4.png" draggable="true" ondragstart="drag(event)" width="80" height="80">
+
+
+</body>
+</html>
 </body>
 </html> 
 
