f49bd74be231bea49a9fc7bfe29c51952f506cae
LawrenceChen15 <lawrence.chn15@gmail.com>
2023-01-12 02:11:10 -0500

    Big Changes To Visuals

--- markerApp/frontEnd/Class.php
+++ markerApp/frontEnd/Class.php
@@ -1,13 +1,4 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<!DOCTYPE html>
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -55,83 +46,89 @@ body {
 <body>
 
 <div class="sidenav">
-  <a href="#about">About</a>
+  <a href="ForgetPasswordPage.html">About</a>
   <a href="#services">Services</a>
   <a href="#clients">Clients</a>
   <a href="#contact">Contact</a>
 </div>
-   
 
-<head>
 <style>
-.grid-container {
-  display: grid;
-  row-gap: 50px;
-  grid-template-columns: auto auto auto;
-  background-color: #2196F3;
-  padding: 10px;
-}
-
-.body-center{
+  .body-center{
   margin-left: 13%;
 }
-
-.grid-item {
-  background-color: rgba(255, 255, 255, 0.8);
-  border: 1px solid rgba(0, 0, 0, 0.8);
-  padding: 20px;
-  font-size: 30px;
-  text-align: center;
+.grid-row {
+  display: flex;
 }
 
-#drag1, #drag2, #drag3, #drag4{
-  float:left;
+.grid-cell {
+  width: 100px;
+  height: 70px;
+  border: 1px solid black;
 }
+
 </style>
-</head>
-<body class = "body-center">
 
-<div ondrop="drop(event)" ondragover="allowDrop(event)" class="grid-container">
-  <div class="grid-item"></div>
-  <div class="grid-item"></div>
-  <div class="grid-item"></div>  
-  <div class="grid-item"></div>
-  <div class="grid-item"></div>
-  <div class="grid-item"></div>  
-  <div class="grid-item"></div>
-  <div class="grid-item"></div>
-  <div class="grid-item"></div>  
-</div>
-
+</style>
 <script>
-function allowDrop(ev) {
-  ev.preventDefault();
+function dragstart_handler(ev) {
+ console.log("dragStart");
+ ev.dataTransfer.setData("text", ev.target.id);
+ // Tell the browser both copy and move are possible
+ ev.effectAllowed = "copyMove";
 }
-
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+function dragover_handler(ev) {
+ console.log("dragOver");
+ // Change the target element's border to signify a drag over event
+ // has occurred
+ ev.preventDefault();
 }
-
-function drop(ev) {
+function drop_handler(ev) {
+  console.log("Drop");
   ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
+  // Get the id of drag source element (that was added to the drag data
+  // payload by the dragstart event handler)
+  var id = ev.dataTransfer.getData("text");
+  // Copy the element if the source and destination ids are both "copy"
+  if (id == "src_copy1" || "src_copy2" || "src_copy3" || "src_copy4" && ev.target.id == "dest_copy") {
+   var nodeCopy = document.getElementById(id).cloneNode(true);
+   nodeCopy.id = "newId";
+   ev.target.appendChild(nodeCopy);
+  }
+}
+function dragend_handler(ev) {
+  console.log("dragEnd");
+  ev.dataTransfer.clearData();
 }
 </script>
 </head>
-<body>
-<div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-<br>
-<img id="drag1" src="/markerApp/markerApp/Pictures/Circle1.png" draggable="true" ondragstart="drag(event)" width="69" height="69">
-<img id="drag2" src="/markerApp/markerApp/Pictures/Circle2.png" draggable="true" ondragstart="drag(event)" width="69" height="69">
-<img id="drag3" src="/markerApp/markerApp/Pictures/Circle3.png" draggable="true" ondragstart="drag(event)" width="69" height="69">
-<img id="drag4" src="/markerApp/markerApp/Pictures/Circle4.png" draggable="true" ondragstart="drag(event)" width="80" height="80">
 
+<head>
+<body class = "body-center">
+<div id="dest_copy" ondrop="drop_handler(event);" ondragover="dragover_handler(event);">
+<div id="grid-container">
+  <script>
+    for(var i = 0; i < 12; i++) {
+      var row = document.createElement("div");
+      row.setAttribute("class", "grid-row");
+      for(var j = 0; j < 12; j++) {
+        var cell = document.createElement("div");
+        cell.setAttribute("class", "grid-cell");
+        row.appendChild(cell);
+      }
+      document.getElementById("grid-container").appendChild(row);
+    }
+  </script>
+</div>
+  
+</div>
+
+ <div>
+<img id="src_copy1" src="/markerApp/markerApp/Pictures/Circle1.png" draggable="true"ondragstart="dragstart_handler(event);" ondragend="dragend_handler(event);" width="69" height="69">
+<img id="src_copy2" src="/markerApp/markerApp/Pictures/Circle2.png" draggable="true" ondragstart="dragstart_handler(event);" ondragend="dragend_handler(event);" width="69" height="69">
+<img id="src_copy3" src="/markerApp/markerApp/Pictures/Circle3.png" draggable="true" ondragstart="dragstart_handler(event);" ondragend="dragend_handler(event);" width="69" height="69">
+<img id="src_copy4" src="/markerApp/markerApp/Pictures/Circle4.png" draggable="true" ondragstart="dragstart_handler(event);" ondragend="dragend_handler(event);" width="80" height="80">
+ </div>
 
 </body>
-</html>
-</body>
-</html> 
-
-</body>
+</head>
 </html>
